9a82a7bc8584588acee540463816fd8fa1f311f4
i.borentegonzalez <i.borentegonzalez@accenture.com>
2024-02-06 16:04:05 +0100

    changed folder structure

---
+++ BackEnd/pipeline_config.groovy
@@ -0,0 +1,14 @@
+/*
+  specify which libraries to load: 
+    In the Governance Tier configuration file, 
+    these should be configurations common across 
+    all apps governed by this config. 
+*/
+libraries{
+  java{
+    configServer = "esto es Java"
+  }
+  node{
+    configServer = "esto es Node"
+  }
+}
